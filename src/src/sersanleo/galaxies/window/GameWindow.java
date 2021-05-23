493e43d982d402c03b5af89697e06abec49ea903
sersanleo <sersanleo@alum.us.es>
2021-05-23 23:41:59 +0200

    Envío 1 - Preguntar antes de cerrar la ventana

--- src/src/sersanleo/galaxies/window/GameWindow.java
+++ src/src/sersanleo/galaxies/window/GameWindow.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -34,7 +36,7 @@ import src.sersanleo.galaxies.window.content.AppContent;
 import src.sersanleo.galaxies.window.content.BoardCreatorPanel;
 import src.sersanleo.galaxies.window.content.GamePanel;
 
-public class GameWindow extends JFrame implements ActionListener {
+public class GameWindow extends JFrame implements ActionListener, WindowListener {
 	private static final long serialVersionUID = 1L;
 
 	private static final int MIN_WIDTH = 400;
@@ -140,7 +142,8 @@ public class GameWindow extends JFrame implements ActionListener {
 		setSize(MIN_WIDTH, MIN_HEIGHT);
 		setResizable(false);
 		setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addWindowListener(this);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		pack();
 		setLocationRelativeTo(null);
 	}
@@ -261,5 +264,36 @@ public class GameWindow extends JFrame implements ActionListener {
 			config.setBoardScale(1.25f);
 		else if (eventSource == scale1_5MenuItem)
 			config.setBoardScale(1.5f);
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		if (this.content != null && !this.content.canBeRemoved())
+			return;
+		System.exit(0);
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
 	}
 }

669d04c79b01f084e8413620f0ffcc4ccfd78707
sersanleo <sersanleo@alum.us.es>
2021-06-20 18:03:40 +0200

    Lo k llevo hasta ahora

--- src/src/sersanleo/galaxies/window/dialog/InfoDialog.java
+++ src/src/sersanleo/galaxies/window/dialog/InfoDialog.java
@@ -5,6 +5,8 @@ import static src.sersanleo.galaxies.util.SwingUtil.link;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -22,19 +24,19 @@ public class InfoDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
 
 	private final JTabbedPane tabbedPane;
+	private final ScrollHelper scrollHelper = new ScrollHelper();
 
 	public InfoDialog(GameWindow window) {
-		super(window, "Información sobre Galaxias", true);
+		super(window, "Información sobre Galaxias", false);
 
 		tabbedPane = new JTabbedPane();
 
 		tabbedPane.addTab("Información", tutorialPanel());
-		tabbedPane.addTab("Ejemplos", examplesPanel());
 
 		add(tabbedPane);
 
-		setResizable(false);
-		setPreferredSize(new Dimension(500, 500));
+		setResizable(true);
+		setPreferredSize(new Dimension(500, 480));
 		pack();
 		setLocationRelativeTo(window);
 	}
@@ -44,10 +46,11 @@ public class InfoDialog extends JDialog {
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
 		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(scrollPane.getViewport().getWidth(), 300));
+		panel.setPreferredSize(new Dimension(scrollPane.getViewport().getWidth(), 420));
 		panel.setBorder(new EmptyBorder(8, 8, 8, 8));
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.addComponentListener(scrollHelper);
 
 		JLabel label = new JLabel(
 				"<html><b>Galaxias</b> es un pasatiempo lógico de origen japonés. Su nombre original, <i>Tentai Show</i>, está formado a partir de la combinación de las palabras “punto”, “simetría” y “astronómica” en su lengua de origen (relacionadas con la estrategia del juego). Fue creado por la compañía editora nipona Nikoli, especializada en juegos, acertijos y rompecabezas lógicos. Nikoli se ha hecho mundialmente conocida gracias a la popularidad de su Sudoku.<br><br>\r\n"
@@ -74,11 +77,22 @@ public class InfoDialog extends JDialog {
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
 		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(scrollPane.getViewport().getWidth(), 300));
+		panel.setPreferredSize(new Dimension(scrollPane.getViewport().getWidth(), 100));
 		panel.setBorder(new EmptyBorder(8, 8, 8, 8));
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
 		scrollPane.getViewport().add(panel);
 		return scrollPane;
+	}
+
+	private final static class ScrollHelper extends ComponentAdapter {
+		public void componentResized(ComponentEvent e) {
+			JPanel panel = (JPanel) e.getSource();
+			Component last = panel.getComponent(panel.getComponentCount() - 1);
+			int height = last.getLocation().y + last.getSize().height
+					+ ((EmptyBorder)panel.getBorder()).getBorderInsets().bottom;
+			panel.setPreferredSize(new Dimension((int) panel.getPreferredSize().getWidth(), height));
+			System.out.println(e.toString());
+		}
 	}
 }
